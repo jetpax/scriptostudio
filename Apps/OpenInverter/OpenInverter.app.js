cc1097f857508537b46cd79f3ced981f46a63c0e
jetpax <jetpax@users.noreply.github.com>
2025-11-14 21:23:43 -0800

    chore(OpenInverter): Bump version to 0.4.0

--- Apps/OpenInverter/OpenInverter.app.js
+++ Apps/OpenInverter/OpenInverter.app.js
@@ -2,7 +2,7 @@
 // {
 //   "name": "OpenInverter",
 //   "id": "openinverter",
-//   "version": [0, 3, 1],
+//   "version": [0, 4, 0],
 //   "author": "JetPax",
 //   "description": "OpenInverter debug and configuration tool for motor control parameters, spot values, CAN mapping, and live plotting",
 //   "icon": "sliders",
